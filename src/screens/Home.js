5f094627560b86e6cfeb540ce359e6e3fff659c9
JeevanPurohit11 <jeevanpurohit11@gmail.com>
2024-05-12 16:25:39 +0530

    initial commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div> <Navbar/> </div>
       <div><Carousel/></div>
       <div className='m-3'>
-        <Card/>
+        <Card/>  
         <Card/>
         <Card/>
         </div>
